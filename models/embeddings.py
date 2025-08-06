bacd840faced0e9b87b64792cd67a74b84e574e8
Aishwarya Tavidisetty <aishwaryatavidisetty@gmail.com>
2025-08-06 23:25:10 +0530

    Switched to OpenAIEmbeddings for cloud compatibility

--- models/embeddings.py
+++ models/embeddings.py
@@ -1,10 +1,12 @@
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from config.config import GROQ_API_KEY 
 
 def embed_documents(texts, index_path='vector_store/faiss_index'):
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     docs = splitter.create_documents(texts)
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": "cpu"})
+    embeddings = OpenAIEmbeddings(openai_api_key=GROQ_API_KEY)  # or your OpenAI key
     vectorstore = FAISS.from_documents(docs, embeddings)
     vectorstore.save_local(index_path)
+
