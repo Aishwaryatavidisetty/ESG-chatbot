1f36f45ca7faac06ec1c133cc656c6f2a46a00a5
Aishwarya Tavidisetty <aishwaryatavidisetty@gmail.com>
2025-08-07 13:40:35 +0530

    Updated ESG Chatbo

--- models/embeddings.py
+++ models/embeddings.py
@@ -1,8 +1,12 @@
 from langchain_community.embeddings import CohereEmbeddings
-from langchain_community.vectorstores import FAISS
-from config.config import COHERE_API_KEY
+from langchain.vectorstores import FAISS
+from config import COHERE_API_KEY
 
-def embed_documents(docs, index_path="vector_store/faiss_index"):
-    embeddings = CohereEmbeddings(cohere_api_key=COHERE_API_KEY, user_agent="esg-chatbot")
-    db = FAISS.from_texts(docs, embedding=embeddings)
+def embed_documents(texts, index_path='vector_store/faiss_index'):
+    embeddings = CohereEmbeddings(
+        cohere_api_key=COHERE_API_KEY,
+        model='embed-english-light-v3.0',
+        user_agent="esg-chatbot"
+    )
+    db = FAISS.from_texts(texts, embedding=embeddings)
     db.save_local(index_path)
