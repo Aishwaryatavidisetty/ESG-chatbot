d31ad0a8704961497d2d05ddd6cf3e26988e27a2
Aishwarya Tavidisetty <aishwaryatavidisetty@gmail.com>
2025-08-07 12:35:48 +0530

    Updated ESG Chatbot to use cohere

--- models/embeddings.py
+++ models/embeddings.py
@@ -1,12 +1,16 @@
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_cohere import CohereEmbeddings
-from config.config import COHERE_API_KEY
+from langchain_community.embeddings import CohereEmbeddings
+from langchain_core.documents import Document
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Make sure env variables are loaded
 
 def embed_documents(texts, index_path='vector_store/faiss_index'):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-    docs = splitter.create_documents(texts)
-    embeddings = CohereEmbeddings(cohere_api_key=COHERE_API_KEY)
+    docs = [Document(page_content=t) for t in texts]
+
+    # ✅ Don't pass the key manually
+    embeddings = CohereEmbeddings()
+
     vectorstore = FAISS.from_documents(docs, embeddings)
     vectorstore.save_local(index_path)
